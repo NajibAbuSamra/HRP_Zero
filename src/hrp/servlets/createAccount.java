ed935aa977611a4fb2e6d98dbacd81e7d7230f6e
Najib <najib.as1990@gmail.com>
2018-06-29 13:46:19 +0300

    begin on createAccountServlet

--- src/hrp/servlets/createAccount.java
+++ src/hrp/servlets/createAccount.java
@@ -1,11 +1,20 @@
 package hrp.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import hrp.da.DataAccess;
 
 /**
  * Servlet implementation class createAccount
@@ -34,8 +43,23 @@ public class createAccount extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		Logger logger = Logger.getLogger("GetPortfolios servlet");
+		logger.log(Level.INFO, "doGet: Start...");
+		DataAccess da = new DataAccess();
+		logger.log(Level.INFO, "DA ready");
+	    String data = "";   
+	    StringBuilder builder = new StringBuilder();
+	    BufferedReader reader = request.getReader();
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	        builder.append(line);
+	    }
+	    data = builder.toString();
+	    JsonObject jobj = new Gson().fromJson(data, JsonObject.class);
+
+	    String result = jobj.get("name").getAsString();
+
+	    System.out.println(result); 
 	}
 
 }
