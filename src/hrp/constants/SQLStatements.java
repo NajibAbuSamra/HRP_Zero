73622cb3c860628b7dfc94a0345c43f05a73c5fc
Najib <najib.as1990@gmail.com>
2018-07-02 20:35:55 +0300

    ordered customers by id

--- src/hrp/constants/SQLStatements.java
+++ src/hrp/constants/SQLStatements.java
@@ -3,5 +3,5 @@ package hrp.constants;
 import hrp.da.DataContract;
 
 public class SQLStatements {
-	public final static String selectAllPortfolios = "SELECT * FROM " + DataContract.CustomersTable.TABLE_NAME + ";";
+	public final static String selectAllPortfolios = "SELECT * FROM " + DataContract.CustomersTable.TABLE_NAME + " ORDER BY " + DataContract.CustomersTable.COL_ID + ";";
 }
