9b9f87e990346a89ea74e7a1d4f537390f009266
Unknown <rana1996am@gmail.com>
2018-07-04 19:46:17 +0300

    models again...

--- src/hrp/model/HrPridection.java
+++ src/hrp/model/HrPridection.java
@@ -1,5 +1,30 @@
 package hrp.model;
 
-public class HrPridection {
+import java.util.ArrayList;
 
+public class HrPridection {
+	private ArrayList<Double> pmd;
+	private ArrayList<Double> dmd;
+	private ArrayList<Double> pridection;
+	public ArrayList<Double> getPmd() {
+		return pmd;
+	}
+	public void setPmd(ArrayList<Double> pmd) {
+		this.pmd = pmd;
+	}
+	public ArrayList<Double> getDmd() {
+		return dmd;
+	}
+	public void setDmd(ArrayList<Double> dmd) {
+		this.dmd = dmd;
+	}
+	public ArrayList<Double> getPridection() {
+		return pridection;
+	}
+	public void setPridection(ArrayList<Double> pridection) {
+		this.pridection = pridection;
+	}
+	
+	
+	
 }
--- src/hrp/model/Project_oc.java
+++ src/hrp/model/Project_oc.java
@@ -1,5 +1,37 @@
 package hrp.model;
 
+import java.util.ArrayList;
+
 public class Project_oc {
+	private double actual;
+	private Country count;
+	private Organization orgn;
+	ArrayList<HrPridection> hr_pr;
+	public double getActual() {
+		return actual;
+	}
+	public void setActual(double actual) {
+		this.actual = actual;
+	}
+	public Country getCount() {
+		return count;
+	}
+	public void setCount(Country count) {
+		this.count = count;
+	}
+	public Organization getOrgn() {
+		return orgn;
+	}
+	public void setOrgn(Organization orgn) {
+		this.orgn = orgn;
+	}
+	public ArrayList<HrPridection> getHr_pr() {
+		return hr_pr;
+	}
+	public void setHr_pr(ArrayList<HrPridection> hr_pr) {
+		this.hr_pr = hr_pr;
+	}
+	
+	
 
 }
