dbb712a321d2ec96f909940126d188d1dc39af66
Unknown <jerby_92@hotmail.com>
2018-07-04 18:16:33 +0300

    fix servlet name

--- src/hrp/servlets/getPredictionInfoByProjectID.java
+++ src/hrp/servlets/getPredictionInfoByProjectID.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class getPredictionInfo
  */
-@WebServlet("/getPredictionInfo")
-public class getPredictionInfo extends HttpServlet {
+@WebServlet("/getPredictionInfoByProjectID")
+public class getPredictionInfoByProjectID extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public getPredictionInfo() {
+    public getPredictionInfoByProjectID() {
         super();
         // TODO Auto-generated constructor stub
     }
