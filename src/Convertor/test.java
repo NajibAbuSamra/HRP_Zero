317306ba5105918478480e353cbe8a044240267e
mayas <jerby_92@hotmail.com>
2018-07-11 19:04:17 +0300

    testing uploading file

---
+++ src/Convertor/test.java
@@ -0,0 +1,17 @@
+package Convertor;
+
+import java.io.File;
+
+public class test {
+
+	public static void main(String[] args) throws Exception {
+		 File input = new File("C:\\Users\\mayas\\Desktop\\TsofenPro\\My-CSV-to-Json\\csv2.csv");
+	       File output = new File("C:\\Users\\mayas\\Desktop\\TsofenPro\\My-CSV-to-Json\\Json.json");
+	 
+		CSV2JSON a = new CSV2JSON();
+		a.convert2(input, output);
+		// TODO Auto-generated method stub
+
+	}
+
+}
