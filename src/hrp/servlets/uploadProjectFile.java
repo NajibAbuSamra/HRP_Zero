ab9696d733526831a9ab1b4969ad2f6a2ead7595
Unknown <khalifa.badir@gmail.com>
2018-07-16 18:37:35 +0300

    uploadFile khalifa

--- src/hrp/servlets/uploadProjectFile.java
+++ src/hrp/servlets/uploadProjectFile.java
@@ -1,20 +1,32 @@
 package hrp.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import com.google.gson.Gson;
+
+import hrp.model.Customer;
+import hrp.model.Project;
+
 /**
  * Servlet implementation class uploadProjectFile
  */
 @WebServlet("/uploadProjectFile")
+@MultipartConfig
 public class uploadProjectFile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -38,11 +50,45 @@ public class uploadProjectFile extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String description = request.getParameter("description"); // Retrieves <input type="text" name="description">
-	    Part filePart = request.getPart("file"); // Retrieves <input type="file" name="file">
-	    String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+	    Part filePart = request.getPart("uploadfile"); // Retrieves <input type="file" name="file">
 	    InputStream fileContent = filePart.getInputStream();
-	    //work on file content
-	}
 
+	    String temp = getStringFromInputStream(fileContent);
+	    			    
+	    response.addHeader("Access-Control-Allow-Origin", "*"); 
+	    response.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS"); 
+	    response.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With"); 
+		String json = new Gson().toJson(temp);
+	
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8"); 
+		response.getWriter().write(json);
+	        
+	}
+	
+	// convert InputStream to String (For the sake of testing)
+	private static String getStringFromInputStream(InputStream is) {
+
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		String line;
+		try {
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return sb.toString();
+	}
 }
